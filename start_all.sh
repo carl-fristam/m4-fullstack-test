76133d0d65185f8b2a2dc02baef1d615f0c8f009
carlF <carl.fristam@gmail.com>
2025-12-21 00:50:44 +0100

    added dockerfile to frontend and details to docker yml so frontend and backend runs together.

--- start_all.sh
+++
@@ -1,5 +0,0 @@
-# This script opens the backend and frontend
-echo "Starting Backend..."
-cd backend && source venv/bin/activate && python app.py & 
-echo "Starting Frontend..."
-cd ../frontend && npm run dev
